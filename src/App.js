ad0410e5521bb4b85f1b9204421e41e4edd03dda
swebent <alexander_benteby@hotmail.com>
2021-10-04 02:14:42 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider, StyledEngineProvider } from '@material-ui/core';
+import GlobalStyles from './components/GlobalStyles';
+import theme from './theme';
+import routes from './routes';
 
-function App() {
+const App = () => {
+  const content = useRoutes(routes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {content}
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
-}
+};
 
 export default App;
